5274c528582555c2489ed2f1df6af833bd5de45e
Can Li <clii0001@student.monash.edu>
2019-03-16 23:05:26 +1100

    来自李灿的无聊commit

--- app/src/main/java/com/team15/foodhack19/MainActivity.java
+++ app/src/main/java/com/team15/foodhack19/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Step> steps;
     private ArrayList<Receipe> receipes;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
